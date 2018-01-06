ba5716a71c5b2b6902115858e3c9a6f84634bb43
buzzhaha <boming.fan@gmail.com>
2018-01-06 15:18:47 -0800

    sshtest

--- DB.sql
+++ DB.sql
@@ -14,9 +14,3 @@ CREATE TABLE products (
 
 INSERT INTO products (flavor, price, quantity)
 VALUES ("vanilla", 2.50, 100);
-
-INSERT INTO products (flavor, price, quantity)
-VALUES ("chocolate", 3.10, 120);
-
-INSERT INTO products (flavor, price, quantity)
-VALUES ("strawberry", 3.25, 75);
