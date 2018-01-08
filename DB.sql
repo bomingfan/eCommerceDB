9c6e969d549850681e787811eab04c54f161c642
buzzhaha <boming.fan@gmail.com>
2018-01-08 01:48:20 -0800

    customer

--- DB.sql
+++ DB.sql
@@ -1,16 +1,29 @@
-DROP DATABASE IF EXISTS ice_creamDB;
+DROP DATABASE IF EXISTS bamazon;
 
-CREATE DATABASE ice_creamDB;
+CREATE DATABASE bamazon;
 
-USE ice_creamDB;
+USE bamazon;
 
 CREATE TABLE products (
   id INT NOT NULL AUTO_INCREMENT,
-  flavor VARCHAR(45) NULL,
-  price DECIMAL(10,2) NULL,
-  quantity INT NULL,
+  product_name VARCHAR(30),
+  department_name VARCHAR(30),
+  denomination DECIMAL(10,2),
+  price DECIMAL(10,2),
+  stock_quantity INT NULL,
   PRIMARY KEY (id)
 );
 
-INSERT INTO products (flavor, price, quantity)
-VALUES ("vanilla", 2.50, 100);
+INSERT INTO products (product_name, department_name, denomination, price, stock_quantity)
+VALUES  ("iTunes Gift Card", "Apple", 10, 10.99, 50), 
+        ("iTunes Gift Card", "Apple", 15, 15.99, 50),
+        ("iTunes Gift Card", "Apple", 25, 25.99, 50), 
+        ("iTunes Gift Card", "Apple", 50, 50.99, 50),
+        ("iTunes Gift Card", "Apple", 100, 100.99, 30), 
+        ("Google Play Gift Card", "Google", 10, 11.99, 20),
+        ("Google Play Gift Card", "Google", 25, 27.99, 20), 
+        ("Google Play Gift Card", "Google", 50, 53.99, 20),
+        ("LOL Game Card", "Riot", 10, 11.99, 60), 
+        ("LOL Game Card", "Riot", 25, 26.99, 60),
+        ("XBOX Gift Card", "Microsoft", 15, 14.99, 100), 
+        ("XBOX Gift Card", "Microsoft", 50, 49.99, 100);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,71 @@
-test
+// require npm
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+// connect database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "19831030",
+    database: "bamazon"
+});
+
+// connect sql server run functions and then end connection
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    // console.log("connected as id " + connection.threadId);
+    userBuy();
+    connection.end();
+});
+
+
+// function prompt user to buy w/ two questions
+function userBuy() {
+
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+    inquirer.prompt([
+        {
+            name: "buy",
+            type: "input",
+            message: "What would you like to buy? Please type an ID number."
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many do you want?",
+            validate: function(value) {
+                if (value > res[3].stock_quantity) {
+                  return true;
+                }
+                // return false;
+                console.log("\nInsufficient quantity! ");
+              }
+        }
+    ]).then(function(ans){
+        console.log(res[ans.buy].stock_quantity);
+    });
+});
+}
+
+// Console log the whole table
+function queryAll() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        for (i = 0; i < res.length; i++) {
+            console.log("id: " + res[i].id +
+                " | Name: " + res[i].product_name +
+                " | Denomination: " + res[i].denomination +
+                " | Price: " + res[i].price +
+                " | Quantity: " + res[i].stock_quantity);
+        }
+        console.log("----------------------------------------------------------------------");
+    });
+}
+
+// 
