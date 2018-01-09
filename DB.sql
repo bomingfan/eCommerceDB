25badf703f06bc55c8104825fd4e169dc239435a
buzzhaha <boming.fan@gmail.com>
2018-01-08 22:18:48 -0800

    customer

--- DB.sql
+++ DB.sql
@@ -15,15 +15,15 @@ CREATE TABLE products (
 );
 
 INSERT INTO products (product_name, department_name, denomination, price, stock_quantity)
-VALUES  ("iTunes Gift Card", "Apple", 10, 10.99, 50), 
-        ("iTunes Gift Card", "Apple", 15, 15.99, 50),
-        ("iTunes Gift Card", "Apple", 25, 25.99, 50), 
-        ("iTunes Gift Card", "Apple", 50, 50.99, 50),
-        ("iTunes Gift Card", "Apple", 100, 100.99, 30), 
-        ("Google Play Gift Card", "Google", 10, 11.99, 20),
-        ("Google Play Gift Card", "Google", 25, 27.99, 20), 
-        ("Google Play Gift Card", "Google", 50, 53.99, 20),
-        ("LOL Game Card", "Riot", 10, 11.99, 60), 
-        ("LOL Game Card", "Riot", 25, 26.99, 60),
-        ("XBOX Gift Card", "Microsoft", 15, 14.99, 100), 
-        ("XBOX Gift Card", "Microsoft", 50, 49.99, 100);
+VALUES  ("iTunes Gift Card", "Apple", 10, 10.99, 10), 
+        ("iTunes Gift Card", "Apple", 15, 15.99, 20),
+        ("iTunes Gift Card", "Apple", 25, 25.99, 30), 
+        ("iTunes Gift Card", "Apple", 50, 50.99, 40),
+        ("iTunes Gift Card", "Apple", 100, 100.99, 50), 
+        ("Google Play Gift Card", "Google", 10, 11.99, 60),
+        ("Google Play Gift Card", "Google", 25, 27.99, 70), 
+        ("Google Play Gift Card", "Google", 50, 53.99, 80),
+        ("LOL Game Card", "Riot", 10, 11.99, 90), 
+        ("LOL Game Card", "Riot", 25, 26.99, 100),
+        ("XBOX Gift Card", "Microsoft", 15, 14.99, 110), 
+        ("XBOX Gift Card", "Microsoft", 50, 49.99, 120);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,14 +21,14 @@ connection.connect(function (err) {
     // run the start function after the connection is made to prompt the user
     // console.log("connected as id " + connection.threadId);
     queryAll();
-    userBuy();
-    connection.end();
+    start();
+    // connection.end();
 });
 
 
 // function prompt user to buy w/ two questions
-function userBuy() {
-
+function start() {
+    
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
     inquirer.prompt([
@@ -37,10 +37,10 @@ function userBuy() {
             name: "buy",
             message: "What would you like to buy? Please type an ID number.",
             validate: function(value) {
-                if (isNaN(value) === false) {
+                if (isNaN(value) === false && value >= 1 && value <= res.length){
                   return true;
                 }
-                console.log("\nInvalid Entry!");
+                console.log("\nInvalid Entry! Please enter an ID number between " + res[0].id + " and " + res[res.length-1].id + ".");
                 return false;
               }
         },
@@ -52,13 +52,20 @@ function userBuy() {
                 if (isNaN(value) === false) {
                   return true;
                 }
-                console.log("\nInvalid Entry!");
+                console.log("\nInvalid Entry! Please enter a number.");
                 return false;
               }
         }
     ]).then(function(ans){
+        if (ans.quantity < res[ans.buy-1].stock_quantity) {
+           buy(ans.buy, ans.quantity);
+           setTimeout(buyAgain, 1000);
+        } else {
+            console.log("Insufficient quantity! We only have " + res[ans.buy-1].stock_quantity + " left.");
+            buyAgain();
+        }
+
         
-        console.log(res[ans.buy].stock_quantity);
     });
 });
 }
@@ -77,4 +84,34 @@ function queryAll() {
     });
 }
 
-// 
+// function update the stock_quantity
+function buy(id, quantity) {
+    var query = "UPDATE products SET stock_quantity = stock_quantity - " + quantity + " where id = " + id;
+    connection.query(query, function (err) {
+      if (err) throw err;
+    });
+    connection.query("SELECT * FROM products", function (err, res) {
+        console.log("Thank you for your order of " + quantity + " $" + res[id-1].denomination + " " + res[id-1].product_name);
+        console.log("Your total is $" + (res[id-1].price*quantity).toFixed(2) + ".");
+        console.log("We have " + res[id-1].stock_quantity + " left in stock.");
+    })
+}
+
+// function to ask user if they want to try again
+function buyAgain() {
+    inquirer.prompt([
+        {
+            name: "again",
+            type: "confirm",
+            message: "Would you like to try to buy again?"
+        }
+    ]).then(function(ans){
+        if (ans.again) {
+            start();
+        } else {
+            console.log("Sorry to see you go. Comeback whenever you want!")
+            connection.end();
+        }
+
+    });
+}
